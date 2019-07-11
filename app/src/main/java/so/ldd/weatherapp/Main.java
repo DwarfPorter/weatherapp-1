6f725d1020232c30abfec439b8f142af94691021
Shegai Vyacheslav <v.shegai@netris.ru>
2019-07-11 13:46:31 +0300

    решение задания урока номер 2

--- app/src/main/java/so/ldd/weatherapp/Main.java
+++ app/src/main/java/so/ldd/weatherapp/Main.java
@@ -1,7 +1,11 @@
 package so.ldd.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlaceSelect(View view) {
+        Intent intent = new Intent(this, LocationSelection.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        String city = getResources().getStringArray(R.array.locations)[data.getExtras().getInt("location")];
+
+        TextView cn = (TextView) findViewById(R.id.cityName);
+        cn.setText(city);
     }
 }
