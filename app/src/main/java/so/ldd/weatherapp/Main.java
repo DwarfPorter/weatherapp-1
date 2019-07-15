212c0d6b22e29da4d627ca897787c6d357df8667
Shegai Vyacheslav <v.shegai@netris.ru>
2019-07-15 13:44:50 +0300

    Решение для урока 3

--- app/src/main/java/so/ldd/weatherapp/Main.java
+++ app/src/main/java/so/ldd/weatherapp/Main.java
@@ -1,18 +1,102 @@
 package so.ldd.weatherapp;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
+    private final String tag = "so.ldd.weatherapp";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "stage onStart", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "stage onStop", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "stage onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "stage onPause", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "stage onResume", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(getApplicationContext(), "stage onRestart", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onRestart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Toast.makeText(getApplicationContext(), "stage onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onRestoreInstanceState");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        Toast.makeText(getApplicationContext(), "stage onSaveInstanceState", Toast.LENGTH_SHORT).show();
+        Log.d(tag, "stage onSaveInstanceState");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try {
+            setContentView(R.layout.activity_main);
+
+            TextView tv = (TextView) findViewById(R.id.celcia);
+            String tx = Singleton.getInstance(WeatherData.class).toString() + "c";
+            if (tv != null)
+                tv.setText(tx);
+
+            Toast.makeText(getApplicationContext(), "stage onCreate", Toast.LENGTH_SHORT).show();
+            Log.d(tag, "stage onCreate");
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onPlaceSelect(View view) {
---
+++ app/src/main/java/so/ldd/weatherapp/WeatherData.java
@@ -0,0 +1,14 @@
+package so.ldd.weatherapp;
+
+/**
+ * Created by k on 15.07.19.
+ */
+
+public class WeatherData {
+
+    private int m_temperature = 3;
+
+    public String toString() {
+        return ((Integer)m_temperature).toString();
+    }
+}
